510e733fa3a320b6400cb36567f02c5ba410b42d
Subhomoy Haldar <hungrybluedev@gmail.com>
2021-12-16 18:18:35 +0530

    add generate_pyramid

--- geometry/triangle.v
+++ geometry/triangle.v
@@ -30,5 +30,17 @@ pub fn generate_right_triangle(options ShapeOptions) []string {
 }
 
 pub fn generate_pyramid(options ShapeOptions) []string {
-	return ['']
+	mut lines := []string{cap: options.size}
+	for i in 0 .. options.size {
+		line_length := 2 * i + 1
+		mut line := []byte{cap: options.size}
+		for _ in 0 .. (options.size - i - 1) {
+			line << ` `
+		}
+		for _ in 0 .. line_length {
+			line << options.symbol
+		}
+		lines << line.bytestr()
+	}
+	return lines
 }
