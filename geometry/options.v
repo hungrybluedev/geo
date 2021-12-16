a5686820e43735336b6ab16fe7a4cbb47f7b7a45
Subhomoy Haldar <hungrybluedev@gmail.com>
2021-12-16 18:18:06 +0530

    add shape_map and allowed_shapes

--- geometry/options.v
+++ geometry/options.v
@@ -13,6 +13,17 @@ pub struct ShapeOptions {
 	symbol byte = `*`
 }
 
+pub const (
+	shape_map = {
+		'left-triangle':  GeometricShape.left_triangle
+		'right-triangle': GeometricShape.right_triangle
+		'pyramid':        GeometricShape.pyramid
+		'square':         GeometricShape.square
+		'diamond':        GeometricShape.diamond
+	}
+	allowed_shapes = shape_map.keys()
+)
+
 pub fn generate_shape(shape GeometricShape, options ShapeOptions) []string {
 	return match shape {
 		.left_triangle { generate_left_triangle(options) }
