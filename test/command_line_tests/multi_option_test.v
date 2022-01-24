54d052e7863e91806067fa3b5b8a7347bf5c31a9
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-01-25 01:28:57 +0530

    use VEXE comptime constant

--- test/command_line_tests/multi_option_test.v
+++ test/command_line_tests/multi_option_test.v
@@ -78,7 +78,7 @@ const (
 
 fn test_all_options() {
 	for case in geometry_tests.all_options_test_cases {
-		result := os.execute_or_panic('v run . --shape $case.shape --size $case.size --symbol "$case.symbol"')
+		result := os.execute_or_panic('${@VEXE} run . --shape $case.shape --size $case.size --symbol "$case.symbol"')
 
 		assert result.exit_code == 0
 		assert result.output == case.output
@@ -87,7 +87,7 @@ fn test_all_options() {
 
 fn test_shapes_only() {
 	for case in geometry_tests.shape_only_test_cases {
-		result := os.execute_or_panic('v run . --shape $case.shape')
+		result := os.execute_or_panic('${@VEXE} run . --shape $case.shape')
 
 		assert result.exit_code == 0
 		assert result.output == case.output
