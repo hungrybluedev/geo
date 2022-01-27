938e612913e32f0d7575c2013507bd9b01fd850e
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-01-28 01:35:34 +0530

    make search terms array constant

--- test/command_line_tests/help_text_test.v
+++ test/command_line_tests/help_text_test.v
@@ -1,21 +1,23 @@
 import os
 
+const (
+	output_search_terms = [
+		'Usage',
+		'Description',
+		'Options',
+		'-p, --shape <string>',
+		'-z, --size <int>',
+		'-m, --symbol <string>',
+		'-h, --help',
+		'--version',
+	]
+)
+
 fn test_help() {
 	flags := ['--help', '-h']
 
 	for flag in flags {
 		result := os.execute_or_panic('${os.quoted_path(@VEXE)} run . $flag')
-
-		output_search_terms := [
-			'Usage',
-			'Description',
-			'Options',
-			'-p, --shape <string>',
-			'-z, --size <int>',
-			'-m, --symbol <string>',
-			'-h, --help',
-			'--version',
-		]
 
 		assert result.exit_code == 0
 		for term in output_search_terms {
