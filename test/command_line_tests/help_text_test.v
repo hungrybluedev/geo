7ceaf37a881e9d1d3431a064abfbfb8001b4a3a5
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-01-27 21:32:14 +0530

    test both help flags

--- test/command_line_tests/help_text_test.v
+++ test/command_line_tests/help_text_test.v
@@ -1,22 +1,26 @@
 import os
 
 fn test_help() {
-	result := os.execute_or_panic('${os.quoted_path(@VEXE)} run . --help')
+	flags := ['--help', '-h']
 
-	output_search_terms := [
-		'Usage',
-		'Description',
-		'Options',
-		'-p, --shape <string>',
-		'-z, --size <int>',
-		'-m, --symbol <string>',
-		'-h, --help',
-		'--version',
-	]
+	for flag in flags {
+		result := os.execute_or_panic('${os.quoted_path(@VEXE)} run . $flag')
 
-	assert result.exit_code == 0
-	assert result.output.contains('Usage')
-	for term in output_search_terms {
-		assert result.output.contains(term)
+		output_search_terms := [
+			'Usage',
+			'Description',
+			'Options',
+			'-p, --shape <string>',
+			'-z, --size <int>',
+			'-m, --symbol <string>',
+			'-h, --help',
+			'--version',
+		]
+
+		assert result.exit_code == 0
+		assert result.output.contains('Usage')
+		for term in output_search_terms {
+			assert result.output.contains(term)
+		}
 	}
 }
